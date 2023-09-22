b9398b7a7953a849a6d70e0ea24beceebef5376b
a5a7 <20941871+a5a7@users.noreply.github.com>
2023-09-21 21:29:18 -0700

    Derivative.html Edited

--- js/derivative.js
+++ js/derivative.js
@@ -1,6 +1,6 @@
 document.getElementById("calculate").addEventListener('click', (e) => {
     var tex = HTML.Value("exp");
-    var out = MathParser.parse(tex);
+    var out = MathParser.parseString(tex);
     HTML.ID("out").innerHTML = "$" + out.derivative().simplify().toLatex() + "$";
     MathJax.typeset();
 })
