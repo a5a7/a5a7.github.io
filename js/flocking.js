7c536dfbb5761360c6b15319d2778eb9d038f28c
a5a7 <20941871+a5a7@users.noreply.github.com>
2024-05-21 20:56:48 -0700

    Redid flocking.html + flocking.js
    made flocking better.

--- js/flocking.js
+++ js/flocking.js
@@ -1,36 +1,16 @@
-var width = 600, height = 450;
+var width = 500, height = 400;
 var ca = new Canvas(width, height);
-ca.Init();
+ca.Init("gen-output");
 
-var count = 1000;
 var boids = [];
-var radii = [10, 20, 30];
-var forceWeights = [1, 1, 1, 0];
-var cruisingSpeed = 5;
-var attract = false;
-var attractionPoint = [width/2, height/2];
-
-ca.canvas.addEventListener('mousedown', (e) => {
-    attract = true;
-    attractionPoint = [e.offsetX, e.offsetY];
-});
-
-ca.canvas.addEventListener('mousemove', (e) => {
-    if (attract){
-        attractionPoint = [e.offsetX, e.offsetY];
-    }
-})
-
-document.addEventListener('mouseup', (e) => {
-    attract = false;
-});
+var radii = [18, 40, 100];
 
 class Boid{
     constructor(){
         this.x = Math.round(Math.random() * width);
-        this.y = Math.round(Math.random() * width);
+        this.y = Math.round(Math.random() * height);
         this.id = "#";
-        for (var i = 0; i < 6; i++) this.id += "1234567890abcdef"[Math.floor(Math.random() * 16)];
+        for (var i = 0; i < 6; i++) this.id += "0123456789abcdef"[Math.floor(Math.random()*16)];
         this.velX = 0;
         this.velY = 0;
     }
@@ -39,10 +19,6 @@ class Boid{
         var dist = Math.sqrt(this.velX * this.velX + this.velY * this.velY);
         if (dist == 0) return [0, 0];
         return [this.velX/dist, this.velY/dist];
-    }
-
-    Init(){
-        boids.push(this);
     }
 
     Distance(b){
@@ -54,41 +30,50 @@ class Boid{
             var dist = this.Distance(x);
             if (x.id == this.id){
                 continue;
-            }else if (dist == 0){
-                continue;
             }
             var vel = x.NormalVelocity();
             if (dist <= radii[0]){
                 // separation //
-                this.velX -= (x.x-this.x)/(Math.sqrt(dist));
-                this.velY -= (x.y-this.y)/(Math.sqrt(dist));
+                this.velX -= (x.x-this.x)/(Math.sqrt(dist**0.5));
+                this.velY -= (x.y-this.y)/(Math.sqrt(dist**0.5));
             }else if (dist <= radii[1]){
                 // alignment //
-                this.velX += vel[0];
-                this.velY += vel[1];
-            }else{
+                this.velX += 2*vel[0];
+                this.velY += 2*vel[1];
+            }else if (dist <= radii[2]){
                 // cohesion //
-                this.velX += (x.x-this.x)/(dist*dist);
-                this.velY += (x.y-this.y)/(dist*dist);
+                this.velX += (x.x-this.x)/(this.Distance(x)**2);
+                this.velY += (x.y-this.y)/(this.Distance(x)**2);
             }
         }
-        this.velX /= 5;
-        this.velY /= 5;
+        // cruising
+        var mag = Math.sqrt(this.velX**2+this.velY**2);
+        if (mag < 4){
+            this.velX *= 2/mag;
+            this.velY *= 2/mag;
+        }
+        if (mag > 9){
+            this.velX *= 3/mag;
+            this.velY *= 3/mag;
+        }
+        if (this.x < 0) this.velX += 1;
+        if (this.x > (width)) this.velX -= 1;
+        if (this.y < 0) this.velY += 1;
+        if (this.y > (height)) this.velY -= 1;
+    }
+
+    MoveNext(){
         this.x += this.velX;
         this.y += this.velY;
+        if (this.x > width) this.velX *= 0.05;
+        if (this.x < 0) this.velX *= 0.05;
+        if (this.y > height) this.velY *= 0.05;
+        if (this.y < 0) this.velY *= 0.05;
     }
 
     Draw(){
-        while (this.x < 0) this.x += width;
-        while (this.y < 0) this.y += height;
-        this.x = this.x%width;
-        this.y = this.y%height;
-        ca.DrawRect(this.x, this.y, 2, 2, this.id);
+        ca.DrawRect(this.x, this.y, 3, 3, this.id);
     }
-}
-
-for (var i = 0; i < count; i++){
-    boids.push(new Boid());
 }
 
 setInterval(() => {
@@ -97,6 +82,17 @@ setInterval(() => {
         x.CalculateNext();
     }
     for (var x of boids){
+        x.MoveNext();
         x.Draw();
     }
-}, 4);
+}, 10);
+
+HTML.ID("simulate").addEventListener("click", () => {
+    var number = HTML.Value("boids");
+    while (number > boids.length){
+        boids.push(new Boid());
+    }
+    while (boids.length > number){
+        boids.pop();
+    }
+}); 
