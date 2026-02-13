d64b9415e08586bab3917036372d1cf072880a9f
a5a7 <aryanagrawal8808@gmail.com>
2026-02-12 17:12:45 -0800

    Updated flocking.

--- js/flocking.js
+++ js/flocking.js
@@ -3,16 +3,21 @@ var ca = new Canvas(width, height);
 ca.Init("gen-output");
 
 var boids = [];
+var cohesionAlignmentRange = 50;
+var separationRange = 10;
+var alignmentFactor = 0.075;
+var cohesionFactor = 0.004;
+var separationFactor = 0.1;
+var margin = 15;
 var radii = [18, 40, 100];
 
 class Boid{
     constructor(){
         this.x = Math.round(Math.random() * width);
         this.y = Math.round(Math.random() * height);
-        this.id = "#";
-        for (var i = 0; i < 6; i++) this.id += "0123456789abcdef"[Math.floor(Math.random()*16)];
-        this.velX = 0;
-        this.velY = 0;
+        this.id = "#000";
+        this.velX = Math.random() * 5 - 2.5;
+        this.velY = Math.random() * 5 - 2.5;
     }
 
     NormalVelocity(){
@@ -26,26 +31,41 @@ class Boid{
     }
 
     CalculateNext(){
+        let separationX = 0, separationY = 0;
+        let alignmentX = 0, alignmentY = 0;
+        let cohesionX = 0, cohesionY = 0;
+        let neighbors = 0, separation = 0;
         for (var x of boids){
+            if (x == this) continue;
             var dist = this.Distance(x);
-            if (x.id == this.id){
-                continue;
+            if (dist < separationRange){
+                separationX += (this.x - x.x);
+                separationY += (this.y - x.y);
+                separation++;
             }
-            var vel = x.NormalVelocity();
-            if (dist <= radii[0]){
-                // separation //
-                this.velX -= (x.x-this.x)/(Math.sqrt(dist**0.5));
-                this.velY -= (x.y-this.y)/(Math.sqrt(dist**0.5));
-            }else if (dist <= radii[1]){
-                // alignment //
-                this.velX += 2*vel[0];
-                this.velY += 2*vel[1];
-            }else if (dist <= radii[2]){
-                // cohesion //
-                this.velX += (x.x-this.x)/(this.Distance(x)**2);
-                this.velY += (x.y-this.y)/(this.Distance(x)**2);
+            if (dist < cohesionAlignmentRange){
+                alignmentX += x.velX;
+                alignmentY += x.velY;
+                cohesionX += x.x;
+                cohesionY += x.y;
+                neighbors++;
             }
         }
+        if (isNaN(alignmentX)){
+            console.log(cohesionX, cohesionY, alignmentY, x.vx, x.vy);
+        }
+        if (neighbors > 0){
+            alignmentX /= neighbors;
+            alignmentY /= neighbors;
+            cohesionX /= neighbors;
+            cohesionY /= neighbors;
+            this.velX += (alignmentX - this.velX) * alignmentFactor;
+            this.velY += (alignmentY - this.velY) * alignmentFactor;
+            this.velX += (cohesionX - this.x) * cohesionFactor;
+            this.velY += (cohesionY - this.y) * cohesionFactor;
+        }
+        this.velX += separationX * separationFactor;
+        this.velY += separationY * separationFactor;
         // cruising
         var mag = Math.sqrt(this.velX**2+this.velY**2);
         if (mag < 4){
@@ -56,19 +76,15 @@ class Boid{
             this.velX *= 3/mag;
             this.velY *= 3/mag;
         }
-        if (this.x < 0) this.velX += 1;
-        if (this.x > (width)) this.velX -= 1;
-        if (this.y < 0) this.velY += 1;
-        if (this.y > (height)) this.velY -= 1;
+        if (this.x < margin) this.velX += 0.75;
+        if (this.x > (width-margin)) this.velX -= 0.75;
+        if (this.y < margin) this.velY += 0.75;
+        if (this.y > (height-margin)) this.velY -= 0.75;
     }
 
     MoveNext(){
         this.x += this.velX;
         this.y += this.velY;
-        if (this.x > width) this.velX *= 0.05;
-        if (this.x < 0) this.velX *= 0.05;
-        if (this.y > height) this.velY *= 0.05;
-        if (this.y < 0) this.velY *= 0.05;
     }
 
     Draw(){
