e3d30b652ee4763c18ef10a3405c45c04ec1f10f
a5a7 <aryanagrawal8808@gmail.com>
2026-03-23 22:33:37 -0700

    added ascii art

---
+++ js/ascii.js
@@ -0,0 +1,66 @@
+const video = document.getElementById("camera");
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d", {willReadFrequently: true});
+
+canvas.style.display = "none";
+
+document.getElementById("page-container").appendChild(canvas);
+
+canvas.style.border = "1px solid black";
+
+async function startWebcam(){
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+        video.srcObject = stream;
+    } catch (err){
+        console.log("Error: could not access camera: ", err);
+    }
+};
+
+function characterFromGrayscale(x){
+    const ascii = " `.-':_,^=;+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
+    const darkness = [0, 0.0751, 0.0829, 0.0848, 0.1227, 0.1403, 0.1559, 0.185, 0.2183, 0.2417, 0.2571, 0.2852, 0.2902, 0.2919, 0.3099, 0.3192, 0.3232, 0.3294, 0.3384, 0.3609, 0.3619, 0.3667, 0.3737, 0.3747, 0.3838, 0.3921, 0.396, 0.3984, 0.3993, 0.4075, 0.4091, 0.4101, 0.42, 0.423, 0.4247, 0.4274, 0.4293, 0.4328, 0.4382, 0.4385, 0.442, 0.4473, 0.4477, 0.4503, 0.4562, 0.458, 0.461, 0.4638, 0.4667, 0.4686, 0.4693, 0.4703, 0.4833, 0.4881, 0.4944, 0.4953, 0.4992, 0.5509, 0.5567, 0.5569, 0.5591, 0.5602, 0.5602, 0.565, 0.5776, 0.5777, 0.5818, 0.587, 0.5972, 0.5999, 0.6043, 0.6049, 0.6093, 0.6099, 0.6465, 0.6561, 0.6595, 0.6631, 0.6714, 0.6759, 0.6809, 0.6816, 0.6925, 0.7039, 0.7086, 0.7235, 0.7302, 0.7332, 0.7602, 0.7834, 0.8037, 0.9999];
+    var decimal = 1-(x/256);
+    for (var i = 1; i < ascii.length; i++){
+        if (darkness[i] > decimal){
+            return ascii[i];
+        }
+    }
+    const ascii_chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\"^`'. ";
+    return ascii_chars[Math.min(Math.floor(x/256 * ascii_chars.length), ascii_chars.length-1)];
+}
+
+let lastTime = 0;
+let frameChange = 100; // how fast the frame should change in ms...
+let scaleDown = 5;
+
+function render(currentTime){
+    if ((currentTime - lastTime) >= frameChange){
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        let frame = imageData.data;
+        let frameSize = frame.length;
+        let text = "";
+        for (var i = 0; i < frameSize; i += 4){
+            // i, i+1, i+2, i+3 //
+            var g = (frame[i] + frame[i+1] + frame[i+2])/3;
+            text += `<span style="color: rgb(${frame[i]}, ${frame[i+1]}, ${frame[i+2]})">` + characterFromGrayscale(g) + "</span>";
+            if ((i/4)%canvas.width == (canvas.width-1)) text += "<br />";
+        }
+        document.getElementById("ascii-image").innerHTML = text;
+        ctx.putImageData(imageData, 0, 0);
+        lastTime = currentTime;
+    }
+    requestAnimationFrame(render);
+}
+
+async function start(){
+    await startWebcam();
+    video.onloadedmetadata = () => {
+        canvas.width = Math.round(video.videoWidth/scaleDown);
+        canvas.height = Math.round(video.videoHeight/scaleDown);
+    }
+    requestAnimationFrame(render);
+}
+
+start();
